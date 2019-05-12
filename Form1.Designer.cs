2a2884a577966d8e02fde43739bebd23365985be
Million <joseph.million@hartwiginc.com>
2019-05-11 19:48:11 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace G_Code_Formatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.formatButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(1206, 568);
+            this.openButton.Margin = new System.Windows.Forms.Padding(4);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(100, 28);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(1098, 568);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 28);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "LOAD";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(13, 14);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(659, 547);
+            this.textBox.TabIndex = 2;
+            this.textBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // formatButton
+            // 
+            this.formatButton.Location = new System.Drawing.Point(990, 568);
+            this.formatButton.Margin = new System.Windows.Forms.Padding(4);
+            this.formatButton.Name = "formatButton";
+            this.formatButton.Size = new System.Drawing.Size(100, 28);
+            this.formatButton.TabIndex = 3;
+            this.formatButton.Text = "Format";
+            this.formatButton.UseVisualStyleBackColor = true;
+            this.formatButton.Click += new System.EventHandler(this.FormatButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(686, 14);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(620, 547);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 609);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.formatButton);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.openButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button formatButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace G_Code_Formatter
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog;
+        private string text;
+        private GCodeProgram gprog;
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog = new OpenFileDialog();
+        }
+        private string wordLetterBuilder;
+        private string wordValueBuilder;
+        private Block builderBlock;
+        private Word builderWord;
+
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog.FileName);
+                    text = sr.ReadToEnd();
+                    textBox.Text = text;
+                    var path = openFileDialog.FileName;
+
+                    gprog = new GCodeProgram();
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            loadFile();
+        }
+
+        private void loadFile()
+        {
+            var sr = new StreamReader("C:\\Users\\joseph.million\\Desktop\\Code Formatter Text\\TEST-PROG.MIN");
+            text = sr.ReadToEnd();
+            textBox.Text = text;
+            textBox1.Text = text;
+        }
+
+        private void FormatButton_Click(object sender, EventArgs e)
+        {
+            if (text == null)
+            {
+                loadFile();
+            } 
+            wordLetterBuilder = "";
+            wordValueBuilder = "";
+            builderBlock = new Block();
+            builderWord = new Word();
+            gprog = new GCodeProgram();
+            text = textBox.Text;
+            int parens = 0;
+            if (text != null)
+            {
+
+
+                for (int i = 0; i < text.Length; ++i)
+                {
+
+                    char c = text[i];
+
+
+                    if (parens <= 0)
+                    {
+
+                        if (c == '\n' || c == '\r')
+                        {
+                            //Append Last Block
+                            if (wordValueBuilder != "")
+                            {
+                                builderWord.value = wordValueBuilder;
+                                builderBlock.addWord(builderWord);
+                                wordValueBuilder = "";
+                                wordLetterBuilder = "";
+                                builderWord.empty();
+                                gprog.addBlock(builderBlock);
+                                builderBlock.empty();
+                            }
+                            else if (wordLetterBuilder != "")
+                            {
+                                builderWord.letter = wordLetterBuilder;
+                                builderBlock.addWord(builderWord);
+                                wordValueBuilder = "";
+                                wordLetterBuilder = "";
+                                builderWord.empty();
+                                gprog.addBlock(builderBlock);
+                                builderBlock.empty();
+                            }
+                        }
+                        else if (c == ' ' || c == 9)
+                        {
+                            if (wordLetterBuilder != "")
+                            {
+                                builderWord.letter = wordLetterBuilder;
+                                wordLetterBuilder = "";
+                            }
+                            else if (wordValueBuilder != "")
+                            {
+                                builderWord.value = wordValueBuilder;
+                                builderBlock.addWord(builderWord);
+                                wordValueBuilder = "";
+                            }
+
+                        }
+                        else if (c == ')')
+                        {
+                            if (wordLetterBuilder != "")
+                            {
+                                builderWord.letter = wordLetterBuilder;
+                                wordLetterBuilder = "does this ever get hit??";
+                            }
+                            else
+                            {
+                                wordValueBuilder = " ";
+                            }
+                        }
+                        else if (c == '(')
+                        { 
+                            parens += 1;
+                            wordLetterBuilder += c;
+                        }
+                        else if (isLetter(c))
+                        {
+                            //new word?
+                            if (wordValueBuilder != "" && builderWord.letter != "")
+                            {
+                                builderWord.value = wordValueBuilder;
+                                builderBlock.addWord(builderWord);
+                                builderWord.empty();
+                                wordLetterBuilder += c;
+                            }
+                            else if (builderWord.letter != "" && wordValueBuilder != "")
+                            {
+                                wordValueBuilder += c;
+                            }
+                            else
+                            {
+                                wordLetterBuilder += c;
+                            }
+                        }
+                        else if (isNumber(c))
+                        {
+                            //add value
+                            wordValueBuilder += c;
+                            if (wordLetterBuilder != "")
+                            {
+                                builderWord.letter = wordLetterBuilder;
+                                wordLetterBuilder = "";
+                            }
+                        }
+                        else if (isSymbol(c))
+                        {
+                            wordValueBuilder += c;
+                            if (wordLetterBuilder != "")
+                            {
+                                builderWord.letter = wordLetterBuilder;
+                                wordLetterBuilder = "";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (!isParen(c))
+                        {
+                            // While inParen build untill closed paren
+                            wordLetterBuilder += c;
+                        }
+                        else if (c == '(')
+                        {
+                            parens += 1;
+                            wordLetterBuilder += '(';
+                        }
+                        else if (c == ')')
+                        {
+                            wordLetterBuilder += c;
+                            builderWord.letter = wordLetterBuilder;
+                            builderWord.value = "88888888888";
+                            //builderBlock.addWord(builderWord);
+                            //builderWord.empty();
+                            //wordLetterBuilder = "";
+                            //wordValueBuilder = "";
+                            parens-=1;
+                        }
+                    }
+                }
+
+                //Append Last Block
+                if (wordValueBuilder != "")
+                {
+                    builderWord.value = wordValueBuilder;
+                    builderBlock.addWord(builderWord);
+                    wordValueBuilder = "";
+                    wordLetterBuilder = "";
+                    builderWord.empty();
+                    gprog.addBlock(builderBlock);
+                    builderBlock.empty();
+                }
+                else if (wordLetterBuilder != "")
+                {
+                    builderWord.letter = wordLetterBuilder;
+                    builderBlock.addWord(builderWord);
+                    wordValueBuilder = "";
+                    wordLetterBuilder = "";
+                    builderWord.empty();
+                    gprog.addBlock(builderBlock);
+                    builderBlock.empty();
+                }
+                textBox.Text = gprog.text();
+                MessageBox.Show("Thanks!, check debugger");
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select File");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool isLetter(char c)
+        {
+            if ( c >= 65 && c <= 90)
+            {
+                return true;
+            }
+            else if (c >= 97 && c <= 122)
+            {
+                // Warn of lowercase letters
+                MessageBox.Show("Avoid Lowercase letters");
+                return false;
+            }
+            return false;
+        }
+        private bool isNumber(char c)
+        {
+            if ((c >= 48 && c <= 57) || c == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isSymbol(char c)
+        {
+            switch (c)
+            {
+                case '=':
+                    return true;
+                case '+':
+                    return true;
+                case '-':
+                    return true;
+                case '/':
+                    return true;
+                case '*':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private bool isParen(char c)
+        {
+            if (c == '(' || c == ')')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        class Word
+        {
+            public string letter;
+            public string value;
+
+            public bool isWord(Word word)
+            {
+                if ( this.letter != "" && this.value != "")
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public void empty()
+            {
+                this.letter = "";
+                this.value = "";
+            }
+
+            public string text()
+            {
+                return $"{this.letter}{this.value}";
+            }
+
+            public Word copy()
+            {
+                Word word = new Word();
+                string value = this.value;
+                string letter = this.letter;
+                word.value = value;
+                word.letter = letter;
+                return word;
+                //return (Word) this.MemberwiseClone();
+
+            }
+        }
+
+        class Block
+        {
+            List<Word> words;
+
+            public void addWord(Word word)
+            {
+                if (this.words == null)
+                {
+                    this.words = new List<Word>();
+                }
+                this.words.Add(word.copy());
+            }
+
+            public string text()
+            {
+                string output = "";
+
+                foreach (Word word in this.words)
+                {
+                    if (output != "")
+                    {
+                        output += $" {word.text()}";
+                    }
+                    else
+                    {
+                        output = $"{word.text()}";
+                    }
+                }
+                return output + "\r\n";
+            }
+
+            public Block copy()
+            {
+                Block block = new Block();
+                foreach (Word word in this.words)
+                {
+                    block.addWord(word.copy());
+                }
+                return block;
+            }
+
+            public void empty()
+            {
+                this.words = null;
+                this.words = new List<Word>();
+            }
+        }
+
+        class GCodeProgram
+        {
+            List<Block> program;
+
+            public void addBlock(Block block)
+            {
+                if (this.program == null)
+                {
+                    this.program = new List<Block>();
+                }
+                this.program.Add(block.copy());
+            }
+
+            public string text()
+            {
+                string output = "";
+                foreach (Block block in this.program)
+                {
+                    output += block.text();
+                }
+                return output;
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
